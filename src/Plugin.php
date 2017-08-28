34dc7b5620cd6e6758e4832694ea0c1612d1d4db
twinh <twinhuang@qq.com>
2017-08-28 20:54:25 +0800

    初始化会员插件

---
+++ src/Plugin.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace Miaoxing\Member;
+
+use miaoxing\plugin\BasePlugin;
+
+class Plugin extends BasePlugin
+{
+    /**
+     * {@inheritdoc}
+     */
+    protected $name = '会员';
+
+    /**
+     * {@inheritdoc}
+     */
+    protected $description = '包括会员卡等';
+}
+
