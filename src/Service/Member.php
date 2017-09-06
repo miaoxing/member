24af849a010bc153b3f72294959ec100e9524618
twinh <twinhuang@qq.com>
2017-09-06 22:43:48 +0800

    更新有无会员卡的图文逻辑

--- src/Service/Member.php
+++ src/Service/Member.php
@@ -3,17 +3,29 @@
 namespace Miaoxing\Member\Service;
 
 use miaoxing\plugin\BaseService;
+use Miaoxing\Plugin\Service\User;
 
 class Member extends BaseService
 {
+    protected $members = [];
+
     public function __invoke()
     {
         return wei()->memberRecord();
     }
 
-    public function getMember()
+    /**
+     * @param User $user
+     * @return MemberRecord
+     */
+    public function getMember($user = null)
     {
-        $user = wei()->curUser;
-        return wei()->member()->curApp()->notDeleted()->findOrInit(['user_id' => $user['id']]);
+        $user || $user = wei()->curUser;
+
+        if (!isset($this->members[$user['id']])) {
+            $this->members[$user['id']] = wei()->member()->curApp()->notDeleted()->findOrInit(['user_id' => $user['id']]);
+        }
+
+        return $this->members[$user['id']];
     }
 }
