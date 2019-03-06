6eeee88968d99cff22b5b85e96f941a5f42c6aac
twinh <twinhuang@qq.com>
2019-03-06 16:35:00 +0800

    refactoring: tab => nav

--- resources/views/admin/memberStats/show.php
+++ resources/views/admin/memberStats/show.php
@@ -28,7 +28,7 @@ $view->layout();
 
     <h5 class="stat-title">趋势图</h5>
 
-    <ul class="js-chart-tabs nav tab-underline">
+    <ul class="js-chart-tabs nav nav-underline">
       <li role="presentation" class="nav-item">
         <a href="#receive" class="nav-link active" aria-controls="receive" role="tab" data-toggle="tab">领取数</a>
       </li>
